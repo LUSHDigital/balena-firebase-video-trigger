b62276cb46bf1f8854772c77372d608be587d2ef
Simon-Ince <simon.ince@lush.co.uk>
2019-08-30 16:05:06 +0100

    added debug messages all the way through

--- src/omx_trigger.py
+++ src/omx_trigger.py
@@ -6,9 +6,12 @@ from os import path
 def play_video(PLU="default"):
 
     filepath = "/home/pi/videos/" + PLU + ".mp4"
+    print("filepath is" + filepath)
 
     if not path.exists(filepath):
+        print("can not find file, playing default")
         filepath = "/home/pi/videos/default.mp4"
 
+    print("filepath is now" + filepath)
     print("playing...")
     os.system("omxplayer --display=5" + filepath)
